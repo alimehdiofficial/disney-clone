fac76157a6be3085b840aba318613d45d8f5b044
Ali Mehdi <62172818+alimehdiofficial@users.noreply.github.com>
2021-04-24 02:41:56 +0400

    added mobile responsiveness for videos

--- pages/videos/[id].js
+++ pages/videos/[id].js
@@ -5,13 +5,13 @@ function Videos({ results }) {
   const router = useRouter();
 
   return (
-    <div className="pl-24 relative py-36">
+    <div className="pl-7 md:pl-24 relative py-36">
       <Head>
         <title>{router.query.name}</title>
       </Head>
-      <h2 className="text-3xl mb-5">
+      <h2 className="text-lg md:text-3xl mb-5">
         Videos{" "}
-        <span className="text-xl text-[#a3a3a3] border-l border-[#a3a3a3] pl-[15px] ml-[9px]">
+        <span className="text-lg md:text-xl text-[#a3a3a3] border-l border-[#a3a3a3] pl-[15px] ml-[9px]">
           {router.query.name}
         </span>
       </h2>
@@ -33,13 +33,12 @@ function Videos({ results }) {
         {results.map((result) => (
           <div key={result.id} className="mr-4">
             <iframe
-              width="424"
-              height="278"
               src={`https://www.youtube.com/embed/${result.key}`}
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
               allowFullScreen
+              className="w-[274px] h-[188px] md:w-[424px] md:h-[278px]"
             ></iframe>
             <span className="block pt-4">{result.name}</span>
           </div>
